f70b577b6fff85189aa284957f4ac862782dd599
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2022-07-08 13:09:14 +0100

    prevent arbitrary servers from sending datagram to client while still enabling multiserver connection

--- udp-client.py
+++ udp-client.py
@@ -4,14 +4,22 @@ import socket
 from constants import CHAR_ENCODING, MAX_SIZE_BYTES, SERVER_SOCKET_HOST, SERVER_SOCKET_PORT
 
 s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-s.connect( ( SERVER_SOCKET_HOST, SERVER_SOCKET_PORT ) )
-message = input('Input lowercase sentence:')
-data  = message.encode(CHAR_ENCODING)
-s.send(data)
 
-print( 'OS assigned the address {} to client socket'.format(s.getsockname()) )
+hosts = []
 
-data = s.recv(MAX_SIZE_BYTES)
-text = data.decode(CHAR_ENCODING)
+while True:
+    host = input('Input host address: ')
+    port = int(input('Input host port: '))
+    hosts.append((host, port))
+    message = input('Input lowercase sentence:')
+    data  = message.encode(CHAR_ENCODING)
+    s.sendto(data, (host, port))
 
-print('The server replied with {!r}'.format(text))
+    print( 'OS assigned the address {} to client socket'.format(s.getsockname()) )
+
+    data, address = s.recvfrom(MAX_SIZE_BYTES)
+    text = data.decode(CHAR_ENCODING)
+    if (address in hosts):
+        print('The server {} replied with {!r}'.format(address, text))
+    else:
+        print('message {!r} from unexpcted host {}!'.format(text, address))
