01a2053d8a466171d0b0cb23d5576e28ecdc2e3c
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2022-07-08 07:24:20 +0100

    allow only single server connection to client

--- udp-client.py
+++ udp-client.py
@@ -4,14 +4,14 @@ import socket
 from constants import CHAR_ENCODING, MAX_SIZE_BYTES, SERVER_SOCKET_HOST, SERVER_SOCKET_PORT
 
 s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-
+s.connect( ( SERVER_SOCKET_HOST, SERVER_SOCKET_PORT ) )
 message = input('Input lowercase sentence:')
 data  = message.encode(CHAR_ENCODING)
-s.sendto(data, ( SERVER_SOCKET_HOST, SERVER_SOCKET_PORT ))
+s.send(data)
 
 print( 'OS assigned the address {} to client socket'.format(s.getsockname()) )
 
-data, address = s.recvfrom(MAX_SIZE_BYTES)
+data = s.recv(MAX_SIZE_BYTES)
 text = data.decode(CHAR_ENCODING)
 
-print('The server {} replied with {!r}'.format(address, text))
+print('The server replied with {!r}'.format(text))
